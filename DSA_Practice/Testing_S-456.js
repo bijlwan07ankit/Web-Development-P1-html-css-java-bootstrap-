8af72a48ebd85b815a1eba550b87cfc14c7338a9
Ankit Bijlwan <136173190+bijlwan07ankit@users.noreply.github.com>
2023-11-08 16:41:39 +0530

    test

--- DSA_Practice/Testing_S-456.js
+++ DSA_Practice/Testing_S-456.js
@@ -1,11 +1,15 @@
-let a=[1,2,3];
-let b=[1,2,3];
-if(a==b){
-    console.log("A and B are equal in values");
-}
-else if(a===b){
-    console.log("they share different memory location");
-}
-else{
-   console.log ("tumse naho payega beta!");
-}
+// let a=[1,2,3];
+// let b=[1,2,3];
+// if(a==b){
+//     console.log("A and B are equal in values");
+// }
+// else if(a===b){
+//     console.log("they share different memory location");
+// }
+// else{
+//    console.log ("tumse naho payega beta!");
+// }
+// let n="5";
+// n.padstart(5,"0");
+// console.log(n);
+
