60101a6da718b87ab6176dcb2b3d7bb41c475827
Ankit Bijlwan <136173190+bijlwan07ankit@users.noreply.github.com>
2023-11-10 11:39:45 +0530

    declaring function using arrow  function

--- DSA_Practice/Testing_S-456.js
+++ DSA_Practice/Testing_S-456.js
@@ -14,9 +14,8 @@
 // console.log(n);
 
 // ---------------------------------------------------------------------------------------------
-let num=132456;
-let a=num.toString(2);
-a.split(" ").reverse()
+// let num=132456;
+// console.log(numbetToBinary(n));
 // arr = [];
 //     let EvenSum = 0;
 //     let OddSum = 0;
@@ -33,4 +32,34 @@ a.split(" ").reverse()
     // }
     // arr.Reverse;
     // console.log(arr);
-    
+    // function generatePascalTriangle(row, col) {
+    //     if (col === 0 || col === row) return 1;
+    //     return generatePascalTriangle(row - 1, col - 1) + generatePascalTriangle(row - 1, col);
+    //   }
+      
+    //   const result = generatePascalTriangle(5, 2);
+    //   console.log(result);
+
+
+//  using  normal function declararion 
+//   function add(a,b){
+//     console.log(a+b);
+//   }
+//   using arrow function 
+//    const add2=(a,b)=>console.log(a+b);
+// add(2,3);
+// add2(2,3);
+   
+function checkeven(a,b){
+    if(a%2===0 && b%2===0){
+      console.log(true);
+    }
+    else{
+        console.log(false);
+
+    }
+}
+
+const checkevenodd=(a,b)=>console.log((a%2===0 && b%2===0));
+checkeven(2,8);
+checkevenodd(2,8);
